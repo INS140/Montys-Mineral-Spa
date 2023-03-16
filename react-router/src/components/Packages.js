72e4301f1f823693a67bf86827f5f7109a02fd85
INS140 <IsaacNSaettel@gmail.com>
2023-03-16 03:29:43 -0700

    finished routes

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,19 @@
 
 
-export default function Packages() {
-
+export default function Packages({ packages }) {
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {
+                        packages.map(p => {
+                            return <li>{p}</li>
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
